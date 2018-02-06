040b7f3110610b267d9c482bf2a5e15c528c657e
qsl <qsluong1@gmail.com>
2018-02-06 17:08:26 +0100

    Setup packages

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(3000, () => console.log('The magic happens at port 3000'));
+
+module.exports = app;
