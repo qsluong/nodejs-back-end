2841dbc4d62709fff183b8bf8843dab9ed829a62
QS <qsluong1@gmail.com>
2018-02-25 15:19:13 +0100

    Update and delete collection

--- api/v1/collection.route.v1.js
+++ api/v1/collection.route.v1.js
@@ -13,7 +13,23 @@ collection_create = (req, res) => {
         .catch(error => res.status(400).json(error));
 };
 
+collection_update = (req, res) => {
+
+    Collection.findByIdAndUpdate({ _id: req.params.id }, req.body)
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json(error))
+};
+
+collection_delete = (req, res) => {
+
+    Collection.findByIdAndRemove({ _id: req.params.id })
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json(error))
+};
+
 module.exports = {
     collection_list,
-    collection_create
+    collection_create,
+    collection_update,
+    collection_delete
 };
--- controllers/collection.controller.js
+++ controllers/collection.controller.js
@@ -6,5 +6,7 @@ var collectionRoute = require('../api/v1/collection.route.v1'),
 
 router.get('', auth.token, collectionRoute.collection_list);
 router.post('', auth.token, collectionRoute.collection_create);
+router.put('/:id', auth.token, collectionRoute.collection_update);
+router.delete('/:id', auth.token, collectionRoute.collection_delete);
 
 module.exports = router;
